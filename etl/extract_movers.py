24bf0db679a5eb4cc621c07f8e20a784050675e5
flinder <fridolin.linder@gmail.com>
2016-03-28 16:27:19 -0400

    updated extract_movers

--- etl/extract_movers.py
+++ etl/extract_movers.py
@@ -11,10 +11,11 @@ import io
 import os
 import sys
 import json
-from pprint import pprint
 import glob
 import time
-
+import rarfile
+import datetime
+import ntpath
 
 def is_in(box, point):
     '''
@@ -47,6 +48,8 @@ def update_bb(coord, user_dict):
         user_dict['germany'] = True
     elif is_in(syria, coord):
         user_dict['syria'] = True
+    if user_dict['syria'] and user_dict['germany']:
+        user_dict['selected'] = True
 
     return user_dict
                      
@@ -54,10 +57,9 @@ def update_bb(coord, user_dict):
 if __name__ == '__main__':
 
 
-    INPUT_FILES = glob.glob("/Volumes/My Passport/GEOG597/04/*")
-
+    INPUT_FILES = glob.glob('D:/fridolin_linder/twitter_flow_data/*.rar')
     OUTPUT_FILE = '../data/germany_syria_movers.txt'
-    LOG_FILE = 'data_reduction.log'
+    LOG_FILE = 'extract_movers.log'
 
     # Germany: NE 55.05814, 15.04205 SW 47.27021, 5.86624, 
     # Syria: NE 37.319, 42.384998 SW 32.3106, 35.727001
@@ -67,59 +69,104 @@ if __name__ == '__main__':
     # First step: Generate a list of tweets for each user
     users = {}
     skipped = 0
-    no_coordinates = 0
     selected_users = set()
 
+    # Set path to unrar application
+    rarfile.UNRAR_TOOL = "C:/Users/flinder/Downloads/unrar.exe"
+        
     start = time.time()
     for f in INPUT_FILES:
+        
+        # Counter for tweets without geotag
+        no_coordinates = 0
+                
+        # Open connection to compressed archive
+        compressed_file = rarfile.RarFile(f)
+        
+        # Get the name of the tweetfile
+        original_filename = compressed_file.infolist()[0].filename
+        
+        # Extract compressed file to current dir
+        print "Decompressing {}".format(original_filename)
+        compressed_file.extract(original_filename)
+        print "Done."
 
-        with io.open(f, 'r', encoding='utf-8') as infile , io.open(LOG_FILE, 'a') as logfile:
+        # Open iterator to decompressed tweetfile and logfile
+        with io.open(original_filename, 'r', encoding='utf-8') as infile, io.open(file=LOG_FILE, mode='a') as logfile:
 
+            # Loop through every tweet in infile
+            
             for i,line in enumerate(infile):
-
+                #print line
+                #sys.exit()
+                # Exclude comma lines
                 if not line.startswith('{'):
                     skipped += 1
                     continue
 
+                # Load tweet as dictionary
                 try:
                     tweet = json.loads(line)
                 except ValueError: 
-                    #logfile.write("Line {line_number}: json ValueError error\n".format(line_number=i))
+                    print "Line {line_number}: json ValueError error\n".format(line_number=i)
                     continue
-                 
+                
+                # Skip tweets w/o geotag
                 if tweet['coordinates'] is None:
                     no_coordinates += 1
                     continue
                 
-                
-                # Extrac information
+                # Get current user id
                 id_ = tweet['user']['id_str']
 
+                # Skip if we already know we want this user
                 if id_ in selected_users:
                     continue
 
+                # Extract coordinates
                 coord = tuple(tweet['coordinates']['coordinates'])
                  
+                # If new user, update dictionary with updated entry
                 if id_ not in users:
-                    users[id_] = update_bb(coord, {'germany': False, 'syria': False})
+                    users[id_] = update_bb(coord, {'germany': False, 
+                                                   'syria': False,
+                                                   'selected': False})
                           
                 else:
                     users[id_] = update_bb(coord, users[id_])
                 
-                if users[id_]['germany'] and users[id_]['syria']:
-                    selected_users.update(id_)
-                    
+                # If user has tweets in both bounding boxes append to selected users
+                if users[id_]['selected']:
+                    selected_users.update([id_])
+                
+                # Status update
                 if i % 100000 == 0:
                     print i
-                    print "skipped {}".format(skipped)
-                    print "no coordinates {}".format(no_coordinates)
                     print "Found {} movers".format(len(list(selected_users)))
-
-            print "Took {} seconds".format(time.time() - start)
+            
+            print i
+            
+            # Measure required time for this file
+            t = time.time() - start
+            
+            # Keep track of the files we are done with in case it crashes
+            st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+            fname = ntpath.basename(f)
+            log_entry = '[{}]: Finsihed {}. {} lines in {} seconds. Stats: {} tweets without coordinates in this file, {} movers identified in total\n'.format(st, fname, i, t, no_coordinates, len(list(selected_users)))
+            logfile.write(log_entry)
+            print log_entry
             start = time.time()
 
- 
-
+            #Make a backup of the users dictionary
+            with io.open('user_dict_backup.json', 'w+') as backupfile:
+                backupfile.write(unicode(json.dumps(users)))
+            
+            # Weird but apparently necessary
+            infile.close()
+            
+            # Remove the temporary decompressed file
+            os.remove(original_filename)
+    # Write the final output to disk
     with io.open(OUTPUT_FILE,'w') as outfile:
 
         for user in list(selected_users):
