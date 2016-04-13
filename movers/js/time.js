a6ac28d67acd27f6f65c8156d7cfb96fc3aaefe0
xsunfeng <feng@psu.edu>
2016-04-13 15:06:45 -0400

    time travel: sample; color; style; label

--- movers/js/time.js
+++ movers/js/time.js
@@ -26,6 +26,8 @@ $(document).ready(function(){
 		    time.cntry_val_map[arr[i].key] = i;
 		}
 
+		time.u_ids = time.u_ids.slice(0, 10);
+
 		time.u_ids.forEach(function(u_id) {
 			timedataByUserid.filter(u_id);
 			var arr = timedataByTime.bottom(Infinity);
@@ -34,7 +36,7 @@ $(document).ready(function(){
 				item.symbol = u_id;
 				item.date = arr[i].time;
 				item.cntry = arr[i].cntry;
-				item.price = time.cntry_val_map[item.cntry];
+				item.cntry_val = time.cntry_val_map[item.cntry];
 			    time.trips.push(item);
 			}
 			timedataByUserid.filterAll();	
@@ -42,8 +44,8 @@ $(document).ready(function(){
 
 		// Set the dimensions of the canvas / graph
 		var margin = {top: 30, right: 20, bottom: 30, left: 50},
-		    width = 960 - margin.left - margin.right,
-		    height = 600 - margin.top - margin.bottom;
+		    width = 1280 - margin.left - margin.right,
+		    height = 1280 - margin.top - margin.bottom;
 
 		// Parse the date / time
 		var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse; 
@@ -54,16 +56,16 @@ $(document).ready(function(){
 
 		// Define the axes
 		var xAxis = d3.svg.axis().scale(x)
-		    .orient("bottom").ticks(5);
+		    .orient("bottom").ticks(d3.time.months, 1);
 
 		var yAxis = d3.svg.axis().scale(y)
-		    .orient("left").ticks(5);
+		    .orient("left").ticks(120);
 
 		// Define the line
-		var priceline = d3.svg.line()
+		var line = d3.svg.line()
 		    .x(function(d) { return x(d.date); })
-		    .y(function(d) { return y(d.price); });
-		    
+		    .y(function(d) { return y(d.cntry_val); });
+
 		// Adds the svg canvas
 		var svg = d3.select("#time-container")
 		    .append("svg")
@@ -73,43 +75,78 @@ $(document).ready(function(){
 		        .attr("transform", 
 		              "translate(" + margin.left + "," + margin.top + ")");
 
-		    data = time.trips;
+	    data = time.trips;
 
-		    data.forEach(function(d) {
-				d.date = parseDate(d.date);
-				d.price = +d.price;
-		    });
+	    // q?
+	    data.forEach(function(d) {
+			d.date = parseDate(d.date);
+			d.cntry_val = +d.cntry_val;
+	    });
 
-		    // Scale the range of the data
-		    x.domain(d3.extent(data, function(d) { return d.date; }));
-		    y.domain([0, d3.max(data, function(d) { return d.price; })]); 
+	    // Scale the range of the data
+	    x.domain(d3.extent(data, function(d) { return d.date; }));
+	    y.domain([0, d3.max(data, function(d) { return d.cntry_val; })]); 
 
-		    // Nest the entries by symbol
-		    var dataNest = d3.nest()
-		        .key(function(d) {return d.symbol;})
-		        .entries(data);
+	    // Nest the entries by symbol
+	    var dataNest = d3.nest()
+	        .key(function(d) {return d.symbol;})
+	        .entries(data);
 
-		    // Loop through each symbol / key
-		    dataNest.forEach(function(d) {
+	    // Loop through each symbol / key
+	    dataNest.forEach(function(d) {
 
-		        svg.append("path")
-		            .attr("class", "line")
-		            .attr("d", priceline(d.values)); 
+	    	var c = getRandomColor();
 
-		    });
+	        svg.append("path")
+	            .attr("class", "line")
+	            .attr("d", line(d.values))
+	            .attr("stroke", c)
+				.on("mouseover", function(d) {
+					d3.select(this).moveToFront();
+					d3.select(this).classed("top", true);
+					})
+			    .on("mouseout", function(d) {
+			    	d3.select(this).classed("top", false);
+			    });
 
-		    // Add the X Axis
-		    svg.append("g")
-		        .attr("class", "x axis")
-		        .attr("transform", "translate(0," + height + ")")
-		        .call(xAxis);
+			svg.selectAll("dot")
+				.data(d.values)
+				.enter().append("circle")
+				.attr("r", 3.5)
+				.attr("cx", function(d) { return x(d.date); })
+				.attr("cy", function(d) { return y(d.cntry_val); })
+				.attr("fill", c);
 
-		    // Add the Y Axis
-		    svg.append("g")
-		        .attr("class", "y axis")
-		        .call(yAxis);
+	    });
 
+	    // Add the X Axis
+	    svg.append("g")
+	        .attr("class", "x axis")
+	        .attr("transform", "translate(0," + height + ")")
+	        .call(xAxis);
 
+	    // Add the Y Axis
+	    svg.append("g")
+	        .attr("class", "y axis")
+	        .call(yAxis);
 
+	    d3.select(".y.axis").selectAll(".tick").selectAll("text")
+	    	.text(function(d) { return time.cntrys[d]; });
+
+		d3.selection.prototype.moveToFront = function() {  
+			return this.each(function(){
+				this.parentNode.appendChild(this);
+			});
+		};
+
+	}
+
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
 	}
 });
