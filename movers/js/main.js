97e96b702d53468e4538faae940a8271ac936b1a
xsunfeng <feng@psu.edu>
2016-04-17 17:53:30 -0400

    checkbox

--- movers/js/main.js
+++ movers/js/main.js
@@ -9,11 +9,40 @@ $(document).ready(function(){
 		filter.init();
 		timeTravel.init();
 		map.init();
+		init_btns();
 	});
 
-	$("#slider-user-num").on("change", function(){
-		filter.num_users = this.value;
-		timeTravel.init();
-	});
+	function init_btns(){
+		$("body").on("click", "#filter-language-english", function() {
+			if ($(this).is(":checked")) 
+			{
+				console.log("english checked");
+			} else {
+				console.log("english unchecked");
+			}
+		});
+		$("body").on("click", "#filter-language-chinese", function() {
+			if ($('#filter-language-chinese').is(":checked")) 
+			{
+				console.log("chinese checked");
+			} else {
+				console.log("chinese unchecked");
+			}
+		});
 
+		$("#slider-user-num").on("change", function(){
+			filter.num_users = this.value;
+			timeTravel.init();
+		});
+	};
+
+
+	getRandomColor = function () {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	}
 });
