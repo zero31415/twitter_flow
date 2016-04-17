8a53b59899fbf452693d1ecd7d4c808288fde1d0
xiliu18 <xiliu@psu.edu>
2016-04-17 17:28:17 -0400

    Adds by language filter to filter.js

--- movers/js/filter.js
+++ movers/js/filter.js
@@ -40,10 +40,35 @@ filter.byIndividualSelection = function(user_id) {
    filter.update()
 }
 
-// Get the 'checked' languages, find all users that don't spead these languages,
+// Get the 'checked' languages, find all users that don't speak these languages
+// in their First profile language,
 // append them to excludedUsers and call update
-filter.byLanguage = function() {
+filter.byProfLanguage = function() {
     // Finds all user_ids that don't have language in language list
     filter.currentData = ;
     filter.update();
+
+    //get the checkbox information
+    langUserList = []
+    langStatusFalse = []
+    for language in checkbox:
+        if the language is not checked:
+            langStatusFalse.append(language)
+
+    //get through the json object of users, if the users speaks languages in the exclude list
+    //then add this person to the excludedUsers list
+    for (var key in p) {
+        );
+  }
+}
+
+//Get all the languages in current data and
+filter.byProfLanguage.getAllLang = function(){
+    //get all the languages
+
+}
+
+//Get the 'checked' laguages, find all users that do not have 
+filter.byTweetLanguage = function(){
+
 }
