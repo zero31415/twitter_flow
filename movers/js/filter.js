35d4f144e9965a7b4bcfd68fb9ae4ed0e4bf4741
xiliu18 <xiliu@psu.edu>
2016-04-19 09:20:03 -0400

    Added _getDisLatLon function to calculate great-circle distance from two point coordinates

--- movers/js/filter.js
+++ movers/js/filter.js
@@ -111,6 +111,25 @@ filter.initLanguages = function(){
 
 }
 
+//calculates great-circle distances between the two points (lat, lon)
+//modified from "http://stackoverflow.com/questions/27928/
+//calculate-distance-between-two-latitude-longitude-points-haversine-formula"
+var _getDisLatLon = function(lat1,lon1,lat2,lon2){
+	
+	function deg2rad(deg) {
+  		return deg * (Math.PI/180)
+	}
+	
+	var R = 6371; // Radius of the earth in km
+  	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  	var dLon = deg2rad(lon2-lon1); 
+  	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
+  	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  	var d = R * c; // Distance in km
+  	return d;
+}
 /*
  * =============================================================================
  * Filter functions
